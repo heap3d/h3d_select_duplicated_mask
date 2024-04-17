089d73e0d94b433a5290edc05e69da39d918e809
Dmytro Holub <heap3d@gmail.com>
2024-04-18 01:12:39 +0300

    updated: version removed: legacy logging

--- scripts/select_duplicated_mask.py
+++ scripts/select_duplicated_mask.py
@@ -9,8 +9,6 @@
 
 import modo
 import lx
-import os
-from datetime import datetime
 
 scene = modo.Scene()
 
@@ -44,38 +42,14 @@ userVal_delete_material_mask_name = 'h3d_select_duplicated_delete_material_mask'
 userVal_delete_material_mask_username = 'delete material mask'
 userVal_delete_material_mask_dialogname = 'turn ON to delete material mask'
 
-ENABLE_LOGGING = False
-fileLogName = os.path.join(os.path.dirname(scene.filename), (os.path.splitext(os.path.basename(scene.filename))[0]) +
-                           '_select_duplicated_mask_03'+'.log')  # get log file name
-if ENABLE_LOGGING:
-    with open(fileLogName, 'w') as f_log:
-        print(f'{datetime.now()} : start logging...\n', file=f_log)
-
-
-# print decorator
-def print_log_decorator(func):
-    def wrap(text='', enabled=ENABLE_LOGGING, line_div=''):
-        if enabled:
-            func(text)
-            if line_div != '':
-                func(line_div)
-    return wrap
-
-
-@print_log_decorator
-def print_log(text, enabled=ENABLE_LOGGING, line_div=''):
-    if enabled:
-        with open(fileLogName, 'a') as f_log:
-            print(text, file=f_log)
-
 
 def get_mask_list():
     mtag_mask_list = []
     if compare_by_material_tag:
         # include mask with Polygon Tag Type <Material> only and polygon tag not <(all)>
         for mask_item in scene.renderItem.children(True, 'mask'):
-            tag_type = mask_item.channel('ptyp').get()
-            mtag = mask_item.channel('ptag').get()
+            tag_type = mask_item.channel('ptyp').get()  # type: ignore
+            mtag = mask_item.channel('ptag').get()  # type: ignore
             if tag_type == 'Material' or tag_type == '':
                 if mtag != '(none)' and mtag != '':
                     mtag_mask_list.append(mask_item)
@@ -101,7 +75,6 @@ def get_item_mask(mask_item):
 
 
 def are_mask_equal(mask1, mask2):
-    print_log('  comparing to <{}>'.format(mask2.name))
     childrens1 = len(mask1.children(recursive=False, itemType='mask'))
     if childrens1 != 0:
         return False
@@ -124,23 +97,15 @@ try:
     duplicated_mask_list = set()
     # processed_tag_list = {}
     scene_mask_list = get_mask_list()
-    print_log('scene_mask_list[{}]: <{}>'.format(len(scene_mask_list), scene_mask_list))
     tmp_scene_list = set(scene_mask_list)
-    print_log('tmp_scene_list[{}]: <{}>'.format(len(tmp_scene_list), tmp_scene_list))
     for mask in scene_mask_list:
-        print_log('current mask: <{}>'.format(mask.name))
         if mask in tmp_scene_list:
             tmp_scene_list.remove(mask)
-        print_log('tmp_scene_list remove(mask)[{}]: <{}>'.format(len(tmp_scene_list), tmp_scene_list))
         processed_list = set(tmp_scene_list)
-        print_log('processed_list[{}]: <{}>'.format(len(processed_list), processed_list))
         for compared_mask in processed_list:
-            print_log('compared_mask: <{}>'.format(compared_mask))
             if are_mask_equal(mask, compared_mask):
-                print_log('  --> equal')
                 tmp_scene_list.remove(compared_mask)
                 duplicated_mask_list.add(compared_mask)
-                print_log('tmp_scene_list remove compared_mask[{}]: <{}>'.format(len(tmp_scene_list), tmp_scene_list))
     # clear selection
     scene.deselect()
     # select duplicated masks
@@ -148,10 +113,8 @@ try:
         mask.select()
     if len(duplicated_mask_list) > 0:
         lx.eval('item.editorColor yellow')
-    print_log('{} duplicated mask selected'.format(len(duplicated_mask_list)))
 except LookupError:
-    print_log('Error found.')
+    print('Error found.')
 finally:
-    print_log('\nselect_duplicated_mask.py done.')
     print('')
     print('select_duplicated_mask.py done.')
